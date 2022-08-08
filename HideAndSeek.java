acce6f3c71c5b8820694cdf30e0958548d035a22
doubledogz <quyiming0914@gmail.com>
2022-08-08 21:53:20 +0900

    no message

---
+++ HideAndSeek.java
@@ -0,0 +1,8 @@
+public class HideAndSeek {
+	public static void main(String[] args) {
+		System.out.println("Let's play hide and seek.");
+		System.out.println("One...");
+		System.out.println("Ready or not, Here I come!");
+		
+	}
+}
