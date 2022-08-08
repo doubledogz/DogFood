fcec068b913d51fa13087a0c42d2a24922fa15f7
doubledogz <quyiming0914@gmail.com>
2022-08-08 21:59:09 +0900

    no message

---
+++ Timeline.java
@@ -0,0 +1,11 @@
+public class Timeline {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		
+		System.out.println("You were born in 1995");
+		
+		System.out.println("You were created by Jams Gosling");
+		
+		System.out.println("You are afun language!");
+		}
+}
