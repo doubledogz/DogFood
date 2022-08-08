2befd47b94069f6eca954008e8bae7d2d2944ba5
doubledogz <quyiming0914@gmail.com>
2022-08-08 22:11:36 +0900

    no message

---
+++ LanguageFacts.java
@@ -0,0 +1,8 @@
+public class LanguageFacts {
+	public static void main(String[] args) {	
+		System.out.println("Java is a class-based");
+		System.out.println("Java classes have a 'main'method.");
+		System.out.println("Java statements end with a semicolon.");
+		System.out.println("Programming is cool!");		
+	}
+}
