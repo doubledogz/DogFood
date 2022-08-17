18cdc7138eb2da8b3a571af7c028615595b2ba35
doubledogz <quyiming0914@gmail.com>
2022-08-17 23:23:21 +0900

    8.17

---
+++ MyProfile.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+	public static void main(String[] args) {
+		String name = "Doubledogz";
+		int age = 32;
+		double desiredSalary = 200000.00;
+		char gender = 'm';
+		boolean lookingForJob = true;
+		
+		System.out.println(age);
+	}
+}
