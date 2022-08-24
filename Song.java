e94672e803dd6caa4f1699f1d70af1e96a2e7068
doubledogz <quyiming0914@gmail.com>
2022-08-24 23:06:50 +0900

    8.24

--- Song.java
+++ Song.java
@@ -1,7 +1,10 @@
 public class Song {
 	public static void main(String[] args) {
-		String openingLyrics = "Yesterday,all my troubles seemed so far away";
-		System.out.println(openingLyrics);
+		String line1 = "Nah nah nah nah, yeah";
+		String line2 = "Nah nah nah nah, hey Jude";
+		String line3 = "Nah nah nah nah, hey Jude";
 		
+		System.out.println(line1.equals(line2));
+		System.out.println(line2.equals(line3));
 	}
 }
