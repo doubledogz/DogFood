63edc24df12faf0c4fc05d202352376bf42890b2
doubledogz <quyiming0914@gmail.com>
2022-08-26 23:15:50 +0900

    8.27

---
+++ Car.java
@@ -0,0 +1,18 @@
+class Car {
+	String color;
+	
+	public Car(String carColor) {
+		color = carColor;
+	}
+	
+	public void startEngine() {
+		System.out.println("Starting the car!");
+		System.out.println("Vroom!");
+	}
+	
+	public static void main(String[] args) {
+		Car myFastCar = new Car("red");
+		myFastCar.startEngine();
+		System.out.println("That was one fast car!");
+	}
+}
